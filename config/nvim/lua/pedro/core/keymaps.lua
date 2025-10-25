39161cff3786d3fabdea38a57802de218b900b44
Pedro Jelev <pedroalves2801@gmail.com>
2025-10-25 14:22:29 -0300

    Feat: add keymap to exit terminal

--- config/nvim/lua/pedro/core/keymaps.lua
+++ config/nvim/lua/pedro/core/keymaps.lua
@@ -6,6 +6,8 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
